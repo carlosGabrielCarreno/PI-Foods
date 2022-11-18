4a051dea85a4708157dd2a62182fbcce2538f58f
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-11-17 23:33:06 -0300

    add function

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ const { loadRecipesInTheDb } = require('./src/helpers/loadRecipesInTheDb.js');
 const PORT = process.env.PORT;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(async () => {
-  //await loadRecipesInTheDb();
+conn.sync({ force: true }).then(async () => {
+  await loadRecipesInTheDb();
   server.listen(PORT, () => {
     console.log('%s listening at ' + PORT); // eslint-disable-line no-console
   });
